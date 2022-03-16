0ba30cbbcb806e93cdbb551d42beba23103874b8
eliel <silvaeliel940@gmail.com>
2022-03-16 16:00:19 -0300

    feat: requisito 1 , finalmente

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(idss) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (!ids) {
+    return [];
+  } if (ids.length === 1) {
+    return [data.species.find((item) => ids[0] === item.id)];
+  }
+  return ids.map((item) => data.species.find((specie) => specie.id === item));
 }
 
 module.exports = getSpeciesByIds;
