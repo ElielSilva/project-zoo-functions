e0b6034b7cca4fc71a7a9d2b3eb4dbd03c412a86
eliel <silvaeliel940@gmail.com>
2022-03-17 15:22:10 -0300

    feat: requisito 9, estamos indo com tudo papai!

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
+// console.log(data.species.find((employee) => employee.id === '0938aa23-f153-4937-9f88-4858b24d6bce'));
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstAnimalId = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const firstAnimal = data.species.find((Animal) => Animal.id === firstAnimalId).residents
+    .reduce((acc, curr) => (acc.age > curr.age ? acc : curr));
+  return Object.values(firstAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
