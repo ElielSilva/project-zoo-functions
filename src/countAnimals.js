fc4ea2334eee4967454fef601696641eb7814de3
eliel <silvaeliel940@gmail.com>
2022-03-16 21:38:18 -0300

    feat: requisito 5 , boraaaaaa!

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return data.species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  } if (Object.keys(animal).length === 1) {
+    return data.species.find((obj) => obj.name === animal.specie).residents.length;
+  } return data.species.find((obj) => obj.name === animal.specie).residents
+    .filter((item) => item.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
